bea2aa8d9439e770981038e8e073de140cb7eb88
Renan Sies <renansies@gmail.com>
2024-07-01 22:12:46 -0300

    fix path

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var defaultPath = Environment.GetEnvironmentVariable("ROOT_PATH");
 var destination = Environment.GetEnvironmentVariable("DESTINATION");
 var appName = Environment.GetEnvironmentVariable("APP_NAME");
 
-app.MapGet($"{defaultPath}/", () => $"Hello World! for destination {destination} from {appName} app\n");
+app.MapGet($"/api/foo/", () => $"Hello World! for destination from foo app\n");
 app.MapGet($"{defaultPath}/simulate/status/200", () => $"Successfull request for destination {destination} from {appName} app\n");
 
 app.Run();
